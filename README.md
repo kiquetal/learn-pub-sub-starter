8c19b1f845338839ea5d8c6d2ea89919b5b9c99d
kiquetal <kiquetal@gmail.com>
2024-05-26 22:19:00 -0400

    Adding code to connect to rabbitmq

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learn-pub-sub-starter (Peril)
 
 This is the starter code used in Boot.dev's [Learn Pub/Sub](https://learn.boot.dev/learn-pub-sub) course.
+
+
+# Excute docker
+
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
+
+	conn_url := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conn_url)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ")
+		panic(err)
+	}
 	fmt.Println("Starting Peril server...")
+	defer conn.Close()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT)
+
+	sigs := <-sig
+	fmt.Println("Received signal: ", sigs)
+	fmt.Println("Closing Peril server...")
+
 }
