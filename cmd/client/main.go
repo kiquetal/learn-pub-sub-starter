976beaa3f160727310c936771c54e7ec892ddd08
kiquetal <kiquetal@gmail.com>
2024-06-01 15:33:17 -0400

    Adding the creation of a transient queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"time"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	fmt.Println("Connecting to RabbitMQ...")
+	connUrl := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connUrl)
+	if err != nil {
+		fmt.Println("Failed to connect to RabbitMQ")
+		panic(err)
+	}
+	name, err := gamelogic.ClientWelcome()
+
+	defer func() {
+		if err := conn.Close(); err != nil {
+			fmt.Println("Failed to close connection")
+		}
+	}()
+
+	_, queue, errorBinding := pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, name), routing.PauseKey, pubsub.TransientQueue)
+	if errorBinding != nil {
+		fmt.Println("Failed to declare and bind queue")
+		panic(err)
+	}
+	time.Sleep(50 * time.Second)
+	fmt.Println("Queue created: ", queue.Name)
+
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -3,8 +3,12 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+const DurableQueue = 0
+const TransientQueue = 1
 
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, data T) error {
 	jsonData, err := json.Marshal(data)
@@ -15,4 +19,36 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, data T) error {
 		ContentType: "application/json",
 		Body:        jsonData,
 	})
+}
+
+func DeclareAndBind(conn *amqp.Connection, exchange, queueName, key string, simpleQueueType int) (*amqp.Channel, amqp.Queue, error) {
+
+	channel, err := conn.Channel()
+	if err != nil {
+		fmt.Println("Failed to open a channel")
+		return nil, amqp.Queue{}, err
+	}
+
+	var queue amqp.Queue
+
+	switch simpleQueueType {
+	case DurableQueue:
+		queue, err = channel.QueueDeclare(queueName, true, false, false, false, nil)
+	case TransientQueue:
+		queue, err = channel.QueueDeclare(queueName, false, true, true, false, nil)
+	}
+
+	if err != nil {
+		fmt.Printf("Failed to declare queue: %v\\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	err = channel.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		fmt.Printf("Failed to bind queue: %v\\n", err)
+		return nil, amqp.Queue{}, err
+	}
+
+	return channel, queue, nil
+
 }
